9581da3fd7ba36e3ce1e6f07e8c537772a615f8a
devboy2011 <buituananhit.contact@gmail.com>
2025-11-22 11:41:46 +0700

    extract user info

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-router.get('/profile', userController.getProfile);
-router.put('/profile', userController.updateProfile);
+router.get('/profile', 
+    authMiddleware,
+    userController.getProfile);
+router.put('/profile', 
+    authMiddleware,
+    userController.updateProfile);
 
 module.exports = router;
